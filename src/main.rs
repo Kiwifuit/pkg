1c04f0b4781b770ec1fa6306b924a3c20dd73612
Kiwifuit <mahkiwi123@gmail.com>
2022-09-06 21:58:10 +0800

    Packing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::error::Error;
 use std::fmt::Debug;
+use std::fs::read;
 use std::fs::File;
+use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 use clap::{AppSettings, Parser};
 use glob::glob;
-use zip::ZipWriter;
+use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
 struct Package {
     path: Box<Path>,
+    zip_options: Option<FileOptions>,
 }
 
 impl Package {
@@ -23,17 +26,63 @@ impl Package {
 
         Some(Self {
             path: Box::from(path),
+            zip_options: None,
         })
     }
 
+    fn set_options(&mut self, compression_level: i32, method: String) {
+        let meth = match method.to_lowercase().as_str() {
+            "aes" => CompressionMethod::Aes,
+            "bz2" => CompressionMethod::Bzip2,
+            "deflate" => CompressionMethod::Deflated,
+            "zstd" => CompressionMethod::Zstd,
+            "store" | _ => CompressionMethod::Stored,
+        };
+
+        self.zip_options = Some(
+            FileOptions::default()
+                .compression_level(Some(compression_level))
+                .compression_method(meth),
+        );
+    }
+
     fn package(&self) -> Result<(), Box<dyn Error>> {
-        let file = File::create(&self.path)?;
+        let file = File::create(self.path.to_str().unwrap().to_owned() + ".zip")?;
         let mut writer = ZipWriter::new(file);
         let file_list = self.search()?;
 
+        if let None = self.zip_options {}
+
         for file in file_list {
-            println!("File: {:?}", file);
+            print!(
+                "[{}] {:?}",
+                if file.is_dir() { "D" } else { "F" },
+                file.to_str().unwrap()
+            );
+
+            if file.is_dir() {
+                writer.add_directory(file.to_str().unwrap(), self.zip_options.unwrap())?;
+                print!("\n");
+            } else {
+                print!("...");
+                let contents = read(&file)?;
+
+                writer.start_file(
+                    file.file_name().unwrap().to_str().unwrap(),
+                    self.zip_options.unwrap(),
+                )?;
+
+                let bytes_written = writer.write(&contents)?;
+                print!(
+                    "OK ({}/{} | {:.2}%)\n",
+                    bytes_written,
+                    contents.len(),
+                    safe_divide(bytes_written, contents.len()) * 100
+                );
+            }
         }
+
+        writer.finish()?;
         Ok(())
     }
 
@@ -93,21 +142,43 @@ enum Interface {
     Package {
         #[clap(short = 'n', long = "name")]
         name: Package,
+
+        #[clap(short = 'l', long = "level", default_value_t = 0)]
+        compression_level: i32,
+
+        #[clap(short = 'm', long = "method", default_value = "store")]
+        method: String,
     },
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn safe_divide(a: usize, b: usize) -> usize {
+    if a == 0 || b == 0 {
+        return 0;
+    }
+    a / b
+}
+
+fn main() {
     let cli = Interface::parse();
 
-    println!("{:#?}", cli);
     match cli {
-        Interface::Package { name: pkg } => {
-            println!("Packaging package {:?}", pkg.package_name());
-            for file in pkg.search()? {
-                println!("File {:?}", file);
-            }
+        Interface::Package {
+            name: mut pkg,
+            compression_level: level,
+            method: meth,
+        } => {
+            pkg.set_options(level, meth);
+
+            println!(
+                "Packaging package {:?} with compress level {}",
+                pkg.package_name(),
+                level
+            );
+
+            match pkg.package() {
+                Ok(_) => println!("Package success!"),
+                Err(err) => eprintln!("Error during packaging: {}", err),
+            };
         }
     }
-
-    Ok(())
 }
